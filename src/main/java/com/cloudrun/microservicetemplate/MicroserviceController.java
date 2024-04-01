d009f723d83d37c85c69fa10770345e438fbf07d
marka12233776 <165614453+marka12233776@users.noreply.github.com>
2024-04-01 13:58:38 +1000

    Update MicroserviceController.java

--- src/main/java/com/cloudrun/microservicetemplate/MicroserviceController.java
+++ src/main/java/com/cloudrun/microservicetemplate/MicroserviceController.java
@@ -40,6 +40,6 @@ public class MicroserviceController {
     // Use logger with log correlation
     // https://cloud.google.com/run/docs/logging#correlate-logs
     logger.info("Structured logging example.");
-    return "Hello World!";
+    return "Hello CQUni,  Updated by Mark on 1.4.24!";
   }
 }
